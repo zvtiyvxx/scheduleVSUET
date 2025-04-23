06d8606181c74d7e4a67a69157aa7ea4354d8529
zvtiyvxx <pasha.kalinin.05@mail.ru>
2025-04-23 21:14:49 +0300

    Update setup_scheduler.py

--- app/setup_scheduler.py
+++ app/setup_scheduler.py
@@ -4,13 +4,10 @@ from app.notification import send_notifications
 from parser.auto_parser import download_tables
 from datetime import datetime, timedelta
 
-# Настройка планировщика APScheduler
 def setup(bot):
     scheduler = AsyncIOScheduler()
 
-    # Добавляем задачу: отправка уведомлений каждый день в 19:00
     scheduler.add_job(send_notifications, "cron", hour=19, minute=0, args=[bot], timezone=timezone("Europe/Moscow"))
     scheduler.add_job(download_tables, "cron", hour=1, minute=0, args=[bot], timezone=timezone("Europe/Moscow"))
 
-    # Запускаем планировщик
     scheduler.start()
